d04f549cb6a820bdc2b1724cddd7e6404144818b
studersi <mail@studer.si>
2014-11-19 21:24:34 +0100

    Initial commit.

---
+++ BasicExample.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author simu
+ */
+public class BasicExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create new Signal object
+        Signal<Integer, String> someSignal = new Signal<>();
+        
+        // create new Slot, suitable for the Signal
+        Signal.Slot<Integer, String> slot1 = new Signal.Slot<Integer, String>() {
+            @Override
+            public void call(Integer value1, String value2) {
+                System.out.println("slot1: " + value1 + " " + value2);
+            }
+        };
+        
+        // connect the Slot to the Signal
+        someSignal.connect(slot1);
+        
+        // connect a second Slot to the Signal, implementing the Slot
+        // interface on the spot
+        someSignal.connect(new Signal.Slot<Integer, String>() {
+            @Override
+            public void call(Integer value1, String value2) {
+                System.out.println("slot2: " + value1 + " " + value2);
+            }
+        });
+        
+        // emit the Signal to call both Slots
+        someSignal.emit(1,"Hello");
+        
+    }
+    
+}
---
+++ Signal.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+
+/**
+ * Light weight generic class to broadcast information to controllers.<br/>
+ * When a Signal is emitted, all connected Slots are executed.<br/>
+ * This way of coupling controllers to the model is looser (and easier to use)
+ * than extending Observable and implementing Observer.<br/>
+ * This mimics, to a certain extent, the behavior of the Signals and Slots
+ * in the QT framework.<br/>
+ * This system of Signals and Slots is type safe because the Slot
+ * interface is integrated into the Signal class.
+ * @author simu
+ * @param <K> type of the first transmitted information, usually the index
+ * @param <V> type of the second transmitted information, usually the value
+ */
+public class Signal<K,V> {
+    /**
+     * ArrayList containing all the Slots to be executed.
+     */
+    private final ArrayList<Slot<K,V>> slots = new ArrayList<>();
+
+    /**
+     * Execute all Slots.
+     * @param value1 first information to be transmit
+     * @param value2 second information to be transmit
+     */
+    public void emit(K value1, V value2) {
+        for (Slot<K,V> each : slots) {
+                each.call(value1, value2);
+        }
+    }
+
+    /**
+     * Connect a Slot to a signal.<br/>
+     * Add a Slot to the ArrayList.
+     * @param slot the Slot to be added
+     */
+    public void connect(Slot<K,V> slot) {
+        slots.add(slot);
+    }
+    
+    /**
+     * The interface for the Slot.
+     * @param <K> type of the first transmitted information
+     * @param <V> type of the second transmitted information
+     */
+    public interface Slot<K,V> {
+        public void call(K value1, V value2);
+    }
+}
